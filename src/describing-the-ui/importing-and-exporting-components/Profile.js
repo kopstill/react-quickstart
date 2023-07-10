e2d5a8789a4e49bc619818a20b487737c9a96be2
kopever <kop.still@gmail.com>
2023-07-11 02:35:38 +0400

    importing and exporting components

---
+++ src/describing-the-ui/importing-and-exporting-components/Profile.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 // import QuickStart from "./quick-start/QuickStart";
-import FirstComponent from "./describing-the-ui/FirstComponent";
+// import Gallery from "./describing-the-ui/first-component/Gallery";
+import { Gallery } from "./describing-the-ui/importing-and-exporting-components/Gallery.js";
+import { Profile } from "./describing-the-ui/importing-and-exporting-components/Profile.js";
 
 const root = createRoot(document.getElementById("root"));
 // root.render(<QuickStart />);
-root.render(<FirstComponent />);
+root.render(
+  <div>
+    <Gallery />
+    <Gallery />
+    <Gallery />
+    <hr />
+    <Profile />
+    <Profile />
+    <Profile />
+  </div>
+);

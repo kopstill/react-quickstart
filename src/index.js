eebb9c0747c6e5230f98d25c26ab681f88d4b7e2
kopever <kop.still@gmail.com>
2023-07-11 01:12:36 +0400

    first component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import QuickStart from "./quick-start/QuickStart";
+// import QuickStart from "./quick-start/QuickStart";
+import FirstComponent from "./describing-the-ui/FirstComponent";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<QuickStart />);
+// root.render(<QuickStart />);
+root.render(<FirstComponent />);

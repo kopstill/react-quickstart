2efb7ca4cd668f59c94495e9ff598ae61483ca80
kopever <kop.still@gmail.com>
2023-07-12 01:14:24 +0400

    writing markup with jsx

---
+++ src/describing-the-ui/writing-markup-with-jsx/Bio.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Bio() {
+  return (
+    <React.Fragment>
+      <div className="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p className="summary">
+        You can find my thoughts here.
+        <br />
+        <br />
+        <b>
+          And <i>pictures</i>
+        </b>{" "}
+        of scientists!
+      </p>
+    </React.Fragment>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 // import QuickStart from "./quick-start/QuickStart";
 // import Gallery from "./describing-the-ui/first-component/Gallery";
-import { Gallery } from "./describing-the-ui/importing-and-exporting-components/Gallery.js";
-import { Profile } from "./describing-the-ui/importing-and-exporting-components/Profile.js";
+// import { Gallery } from "./describing-the-ui/importing-and-exporting-components/Gallery.js";
+// import { Profile } from "./describing-the-ui/importing-and-exporting-components/Profile.js";
+import TodoList from "./describing-the-ui/writing-markup-with-jsx/TodoList.js";
+import Bio from "./describing-the-ui/writing-markup-with-jsx/Bio.js";
 
 const root = createRoot(document.getElementById("root"));
 // root.render(<QuickStart />);
 root.render(
+  // <div>
+  //   <Gallery />
+  //   <Gallery />
+  //   <Gallery />
+  //   <hr />
+  //   <Profile />
+  //   <Profile />
+  //   <Profile />
+  // </div>
   <div>
-    <Gallery />
-    <Gallery />
-    <Gallery />
-    <hr />
-    <Profile />
-    <Profile />
-    <Profile />
+    <TodoList />
+    <Bio />
   </div>
 );
